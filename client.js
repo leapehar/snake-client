6b14a84819c417eb1745d13902bf5b27074d5248
Lea Pehar <leapehar@gmail.com>
2021-12-05 06:37:30 +0000

    created file and exported connect function

---
+++ client.js
@@ -0,0 +1,24 @@
+
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.on('data', (data) => {
+    console.log('Message to player: ', data)
+  });
+
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+// connect();
+
+module.exports = { connect };

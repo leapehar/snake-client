574fa59f2bee04fbee99b78cf8703440111dd8da
Lea Pehar <leapehar@gmail.com>
2021-12-05 08:58:47 +0000

    added setupInput and handleUserInput functions, functionality so we can terminate the game using ctrl + c.

--- play.js
+++ play.js
@@ -3,3 +3,28 @@ const { connect } = require("./client.js");
 
 //console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", key => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  const stdout = process.stdout;
+
+  if (key === '\u0003') {
+    stdout.write("Goodbye!\n");
+    process.exit();
+  }
+};
+
+setupInput();
+handleUserInput();

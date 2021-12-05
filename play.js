174610cdeaf13a621470f2d7eb39c3168f526e7b
Lea Pehar <leapehar@gmail.com>
2021-12-05 06:38:07 +0000

    imported connect function from client.js

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
-const net = require("net");
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
+const { connect } = require("./client.js");
 
-  // interpret incoming data as text
-  conn.on('data', (data) => {
-    console.log('Message to player: ', data)
-  });
-
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
-console.log("Connecting ...");
+//console.log("Connecting ...");
 connect();
